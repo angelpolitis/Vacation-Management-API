61f15a05b5b53afb28439ee7655ede5e4a2dc494
Angel Politis <info@angelpolitis.com>
2025-11-16 07:44:33 +0200

    Added the necessary constants and function to establish a PDO connection in the bootstrap file.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,18 @@
 <?php
     namespace App;
 
+    use PDO;
+    use PDOException;
+
+    const SRC = __DIR__;
+
+    const DB_PORT = 3306; 
+    const DB_USER = "angel";
+    const DB_PASS = "12345678";
+    const DB_NAME = "vacation_management_api";
+
+    define("App\\DB_HOST", getenv("IS_DOCKER") ? "db" : "127.0.0.1");
+
     # Register a autoloader that includes modules whose namespace follows folder structure.
     spl_autoload_register(function ($class) {
         $classParts = preg_split("#\\\\|\/#", $class);
@@ -16,8 +28,35 @@
         }
 
         # Assume the class is a command.
-        $path = __DIR__ . "/commands/" . end($classParts);
+        $path = __DIR__ . "/commands/" . implode(DIRECTORY_SEPARATOR, array_slice($classParts, 2)) . ".php";
 
         if (is_file($path)) require_once $path;
     });
+
+    /**
+     * Attempts to establish a database connection via PDO.
+     * @throws PDOException If the operation fails.
+     */
+    function getDBConnection () : PDO {
+        static $pdo = null;
+
+        if ($pdo === null) {
+            try {
+                $pdo = new PDO(
+                    "mysql:host=" . DB_HOST .";dbname=" . DB_NAME . ";charset=utf8mb4;port=" . DB_PORT,
+                    DB_USER,
+                    DB_PASS,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    ]
+                );
+            }
+            catch (PDOException $e) {
+                die("Could not connect to the database: " . $e->getMessage());
+            }
+        }
+
+        return $pdo;
+    }
 ?>
