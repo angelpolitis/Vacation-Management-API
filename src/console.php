fb76c5eba2e3861dbea0b3e15c3a00def4925129
Angel Politis <info@angelpolitis.com>
2025-11-16 05:38:12 +0200

    Created the console file that executes the commands provided by the command line interface and a registry file for commands.

---
+++ src/console.php
@@ -0,0 +1,55 @@
+<?php
+    namespace App;
+
+    require_once __DIR__ . "/bootstrap.php";
+
+    use App\Console\Console;
+    use Exception;
+
+    # Terminate the script if the file isn't accessed via the command line.
+    if (PHP_SAPI != "cli") {
+        echo "This entry point can only be accessed via the command line.";
+        return;
+    }
+
+    # Terminate the script if there are too few arguments.
+    if ($argc <= 1) {
+        echo "This entry point requires at least two arguments.";
+        return;
+    }
+
+    $console = new Console(["verbose" => true]);
+
+    $args = array_slice($argv, 2);
+
+    $commandsRegistryPath = file_get_contents(__DIR__ . "/registry/commands.json");
+    $commandsRegistry = json_decode($commandsRegistryPath, true);
+    $command = $commandsRegistry[$argv[1]] ?? null;
+
+    if (!isset($command)) {
+        $console->warn("Unknown command.", true);
+        return;
+    }
+
+    $path = __DIR__ . '/' . $command["file"];
+
+    if (!is_file($path)) {
+        $console->warn("Can't locate the implementation of the command.", true);
+        return;
+    }
+    require_once $path;
+
+    $class = str_replace('.', "\\", $command["class"]);
+
+    if (!class_exists($class, false)) {
+        $console->warn("Can't use the implementation of the command.", true);
+        return;
+    }
+
+    try {
+        echo $console->runCommand(new $class($args), implode(" ", array_slice($argv, 1)));
+    }
+    catch (Exception $e) {
+        $console->warn($e->getMessage());
+    }
+?>
